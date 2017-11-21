f15eb3a3e922d3046d7d67c46e122b2fd611edaf
Brad van der Laan <bvanderl@opentext.com>
2017-11-21 17:07:40 -0500

    Extracted terminal writing to reporter module. This change set pulls out all the spread out console.log/process.std.write lines and put them all in a single reporter module. This way we can write up different types of reporters and effect how we report test results to the user.
    Right now this change has no behavioural differances for the user.

--- test/measure.unit.js
+++ test/measure.unit.js
@@ -14,17 +14,17 @@ chaiUse(chaiAsPromised);
 
 const { measure } = require('../lib/measure');
 
+function createLoggerStub() {
+  return {
+    log: sinon.stub(),
+    error: sinon.stub(),
+  };
+}
+
 describe('Measure', () => {
   describe('Benchmarks', () => {
-    beforeEach(() => {
-      sinon.stub(benchmarks, 'measure').yields();
-      sinon.spy(process.stdout, 'write');
-    });
-
-    afterEach(() => {
-      benchmarks.measure.restore();
-      process.stdout.write.restore();
-    });
+    beforeEach(() => sinon.stub(benchmarks, 'measure').yields());
+    afterEach(() => benchmarks.measure.restore());
 
     it('should run benchmarks', () => {
       const theSuite = new Suite('name');
@@ -41,9 +41,10 @@ describe('Measure', () => {
         minSamples: 200,
         maxTime: 20,
       });
+      const reporter = createLoggerStub();
 
       return theSuite.invokeServiceHooks()
-        .then(() => expect(measure(theSuite))
+        .then(() => expect(measure(theSuite, reporter))
           .to.eventually.be.fulfilled)
         .then(() => {
           expect(benchmarks.measure).to.have.been.calledOnce;
@@ -67,7 +68,9 @@ describe('Measure', () => {
         maxTime: 20,
       });
 
-      return expect(measure(theSuite))
+      const reporter = createLoggerStub();
+
+      return expect(measure(theSuite, reporter))
         .to.eventually.be.fulfilled
         .then(() => {
           expect(benchmarks.measure).to.have.not.been.called;
@@ -85,8 +88,10 @@ describe('Measure', () => {
         maxTime: 20,
       });
 
+      const reporter = createLoggerStub();
+
       return theSuite.invokeServiceHooks()
-        .then(() => expect(measure(theSuite))
+        .then(() => expect(measure(theSuite, reporter))
           .to.eventually.be.fulfilled)
         .then(() => {
           expect(benchmarks.measure).to.have.not.been.called;
@@ -103,8 +108,10 @@ describe('Measure', () => {
         maxMean: 0.2, // 200ms
       });
 
+      const reporter = createLoggerStub();
+
       return theSuite.invokeServiceHooks()
-        .then(() => expect(measure(theSuite))
+        .then(() => expect(measure(theSuite, reporter))
           .to.eventually.be.fulfilled)
         .then(() => {
           expect(benchmarks.measure).to.have.been.calledOnce;
@@ -115,15 +122,8 @@ describe('Measure', () => {
   });
 
   describe('Display Test Starts', () => {
-    before(() => {
-      sinon.stub(benchmarks, 'measure').yields();
-      sinon.spy(process.stdout, 'write');
-    });
-
-    after(() => {
-      benchmarks.measure.restore();
-      process.stdout.write.restore();
-    });
+    before(() => sinon.stub(benchmarks, 'measure').yields());
+    after(() => benchmarks.measure.restore());
 
     it('should print out that the suite has started', () => {
       const theSuite = new Suite('name');
@@ -141,26 +141,21 @@ describe('Measure', () => {
         maxTime: 20,
       });
 
+      const reporter = createLoggerStub();
+
       return theSuite.invokeServiceHooks()
-        .then(() => expect(measure(theSuite))
+        .then(() => expect(measure(theSuite, reporter))
           .to.eventually.be.fulfilled)
         .then(() => {
-          expect(process.stdout.write).to.have.been.called;
-          expect(process.stdout.write).to.have.been.calledWith('  name\n');
+          expect(reporter.log).to.have.been.called;
+          expect(reporter.log).to.have.been.calledWith('name');
         });
     });
   });
 
   describe('Display test ended', () => {
-    before(() => {
-      sinon.stub(benchmarks, 'measure').yields();
-      sinon.spy(process.stdout, 'write');
-    });
-
-    after(() => {
-      benchmarks.measure.restore();
-      process.stdout.write.restore();
-    });
+    before(() => sinon.stub(benchmarks, 'measure').yields());
+    after(() => benchmarks.measure.restore());
 
     it('should print out that the suite has ended', () => {
       const theSuite = new Suite('name');
@@ -178,26 +173,21 @@ describe('Measure', () => {
         maxTime: 20,
       });
 
+      const reporter = createLoggerStub();
+
       return theSuite.invokeServiceHooks()
-        .then(() => expect(measure(theSuite))
+        .then(() => expect(measure(theSuite, reporter))
           .to.eventually.be.fulfilled)
         .then(() => {
-          expect(process.stdout.write).to.have.been.called;
-          expect(process.stdout.write).to.have.been.calledWith('  Done\n');
+          expect(reporter.log).to.have.been.called;
+          expect(reporter.log).to.have.been.calledWith('Done');
         });
     });
   });
 
   describe('Display test errors', () => {
-    before(() => {
-      sinon.stub(benchmarks, 'measure').yields('boo');
-      sinon.spy(process.stdout, 'write');
-    });
-
-    after(() => {
-      benchmarks.measure.restore();
-      process.stdout.write.restore();
-    });
+    before(() => sinon.stub(benchmarks, 'measure').yields('boo'));
+    after(() => benchmarks.measure.restore());
 
     it('should print out that the suite has ended with errors', () => {
       const theSuite = new Suite('name');
@@ -215,12 +205,14 @@ describe('Measure', () => {
         maxTime: 20,
       });
 
+      const reporter = createLoggerStub();
+
       return theSuite.invokeServiceHooks()
-        .then(() => expect(measure(theSuite))
+        .then(() => expect(measure(theSuite, reporter))
           .to.eventually.be.rejected)
         .then(() => {
-          expect(process.stdout.write).to.have.been.called;
-          expect(process.stdout.write).to.have.been.calledWith('\x1b[31m  Done with errors: Error: boo\n\x1b[39m');
+          expect(reporter.error).to.have.been.called;
+          expect(reporter.error).to.have.been.calledWith('Done with errors: Error: boo');
         });
     });
   });
